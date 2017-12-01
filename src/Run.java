b185d9d8d9158f7a60c17e7e6630934b64152acf
apcsa <apcsa@10.254.5.61>
2017-12-01 13:04:05 -0800

    added these to git repo

---
+++ src/Run.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Run {
+	
+	Instructions();
+	
+
+}
+
+
+	private void Instructions()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("shhhhhhhh! this game requires you to be sneaky and quiet");
+		System.out.println(" Can you do that ?");
+		String canYou=sc.next();
+		
+		
+		
+	}
