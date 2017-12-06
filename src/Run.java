368689d7b2b3b9005bad1460c09cfb28685458e6
apcsa <apcsa@10.42.109.34>
2017-12-06 13:05:43 -0800

    added new things december 6

--- src/Run.java
+++ src/Run.java
@@ -1,29 +1,74 @@
 import java.util.Random;
 import java.util.Scanner;
 public class Run {
+	Scanner sc = new Scanner(System.in);
 	
-	Instructions();
-	
-
-}
-
-
-	private void Instructions()
+		private void Instructions()
 	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("shhhhhhhh! this game requires you to be sneaky and quiet");
+		
+		System.out.println("shhhhhhhh! You have been abducted by aliens and are on board an alien spaceship");
+		System.out.println(" The only way to survive now is by escaping");
+		System.out.println(" Now you are currently tied down laying on a flat platform");
+		System.out.println(" You must struggle to set loose whatever alien thing that is tieing you down. Oh nvm it just rope ://");
+		
 		System.out.println(" Can you do that ?");
 		String canYou=sc.next();
-		
-		if userYes()
+		if (userYes() )
 		{
 			System.out.println(" Well let's begin");
+			System.out.println(" Best be ready to run foryour life !!!");
+			
+			
+		}
+		else
+		{
+			death
 		}
 	}
 	
-	private void Bedroom()
-	{
-		System.out.println(" Youy are now in your bedroom \nIf you wish to leave his house wiithout waking up your parents \nYou wil need to be clever and not wake them up" );
 		
+		
+	private boolean userYes()
+	
+	{
+	return	(sc.nextLine().equals("yes")) ;
+			
+	}
+	{
+		return if(sc.nextLine.equals("yes"));
 		
 	}
+
+	private void death();
+	{
+		System.out.println("Congratulations you lil idiot. You were supposed succesfully run away from this alien spaceship but you have failed.");
+		System.out.println("Do you want to play again? Enter Y for yes or N for no!");
+
+        answer = in.nextLine();
+        answer = answer.toLowerCase(); //change input to lowercase for bullet proofing
+        if(answer.equals("y"))
+        {
+        done = false;
+        player = "X";
+        moveCounter = 0;
+        game.resetTheGame();
+
+        }
+
+      else
+         {
+         System.exit(0); //program termination
+       }
+
+
+}
+
+}
+	}
+
+}
+
+	
+
+	
+
